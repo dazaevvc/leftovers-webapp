6a694835a8f8bc8e6ff9f8d0d778fe353272f5df
thrakc3001 <cainmusicteach@gmail.com>
2017-10-16 09:56:14 -0500

    created food schema

---
+++ models/food.js
@@ -0,0 +1,26 @@
+//REQUIRE MONGOOSE CONST
+const mongoose = require("mongoose");
+
+//SCHEMA HANDLER
+const Schema = mongoose.Schema;
+
+//RESTAURANT SCHEMA
+const FoodSchema = new Schema ({
+  name: {
+    type: String,
+    required: true
+  },
+  weight: {
+    type: String,
+    required: true
+  },
+  datePrepared: {
+    type: Date,
+    required: true
+  }
+})
+
+//RESTAURANT MODEL EXPORT
+const Food = mongoose.model("Food", FoodSchema);
+
+module.exports = Food;
