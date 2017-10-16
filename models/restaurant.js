ef08a68c8235960300d17b1d6462c7e84f4f4d4a
Dazaev Vives <dazaev.vives.cervantes@gmail.com>
2017-10-15 19:11:30 -0500

    created fields for the restaurant schema

--- models/restaurant.js
+++ models/restaurant.js
@@ -4,5 +4,26 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 //RESTAURANT SCHEMA
 const restaurant = new Schema ({
-  
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  address: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  foodLeft:
+    [String],
+  phoneNum: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  }
 })
