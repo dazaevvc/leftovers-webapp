448759325e353e73e5dd09cf03e09d2077b2096d
thrakc3001 <cainmusicteach@gmail.com>
2017-10-18 15:24:33 -0500

    created signup.js

---
+++ public/scripts/signup.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+  $("#restForm").on('submit', function(e) {
+    e.preventDefault();
+    $.ajax({
+      method: 'POST',
+      url: 'http://localhost:3000/api/restaurants',
+      data: $(this).serialize(),
+      success: onSuccess,
+      error: Error
+    });
+  });
+});
+
+function onSuccess(json) {
+	console.log(json);
+  $('#restForm input').val('');
+  restaurants.push(json);
+  render();
+};
+
+function onError(data) {
+	console.log('ya dummy');
+};
+
+
+
+
+
+
+
+
+
