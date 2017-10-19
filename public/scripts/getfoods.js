6170baf331ba2ff4d1b33f725f255f5b64d2e9db
miguel mejia <mejiamiguel4@gmail.com>
2017-10-19 14:43:16 -0500

    foodsavailable ejs jquery finished and connected to other pages

---
+++ public/scripts/getfoods.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+  $.ajax({
+    method: 'GET',
+    url: 'http://localhost:3000/api/restaurants',
+    dataType: 'json',
+    data: '',
+    success: onSuccess,
+    error: onError
+  });
+});
+
+function onSuccess(data) {
+  console.log(data);
+  for (var i = 0; i < data.length ; i++) {
+    //$('#main').append('<div class = "name"><br>'+data[i].name+'</div>');
+    $('#main').append('<div class = "foodLeft">'+data[i].foodLeft[0].weight+' of '+data[i].foodLeft[0].name+' prepared on '+data[i].foodLeft[0].datePrepared+'</div>');
+     for (var x = 1; x < data[i].foodLeft.length ; x++) {
+       $('#main').append('<div class = "foodLeft">'+data[i].foodLeft[x].weight+' of '+data[i].foodLeft[x].name+' prepared on '+data[i].foodLeft[x].datePrepared+'</div>');
+    };
+
+  };
+}
+
+  function onError(data) {
+  console.log('ya dummy');
+};
+
