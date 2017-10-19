38a45823659e9f10a75ceb40657a7a1c85b13fbb
miguel mejia <mejiamiguel4@gmail.com>
2017-10-19 10:28:20 -0500

    no errors on the post ajax, working, + error prompt

--- public/scripts/restaurants.js
+++ public/scripts/restaurants.js
@@ -15,11 +15,11 @@ function onSuccess(data) {
 	for (var i = 0; i < data.length ; i++) {
 		$('#main').append('<div class = "name">Restaurant: '+data[i].name+'</div><br>');
 		$('#main').append('<div class = "address">Address: '+data[i].address+'</div><br>');
-		$('#main').append('<div class = "foodLeft">Food Available: '+data[i].foodLeft[0].weight+' of '+data[i].foodLeft[0].name+' prepared on '+data[i].foodLeft[0].datePrepared+'</div><br>');
-			for (var x = 1; x < data[i].foodLeft.length ; x++) {
-				$('#main').append('<div class = "foodLeft">'+data[i].foodLeft[x].weight+' of '+data[i].foodLeft[x].name+' prepared on '+data[i].foodLeft[x].datePrepared+'</div><br>');
+		// $('#main').append('<div class = "foodLeft">Food Available: '+data[i].foodLeft[0].weight+' of '+data[i].foodLeft[0].name+' prepared on '+data[i].foodLeft[0].datePrepared+'</div><br>');
+		// 	for (var x = 1; x < data[i].foodLeft.length ; x++) {
+		// 		$('#main').append('<div class = "foodLeft">'+data[i].foodLeft[x].weight+' of '+data[i].foodLeft[x].name+' prepared on '+data[i].foodLeft[x].datePrepared+'</div><br>');
 
-		};
+		// };
 		$('#main').append('<div class = "phoneNum">Phone: '+data[i].phoneNum+'</div><br>');
 		$('#main').append('<div class = "email">Email: '+data[i].email+'</div><br><br><br>');
 	};
