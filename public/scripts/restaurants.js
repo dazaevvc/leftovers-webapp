26cb1429ff9c52b686795da8990f3654e9484e3a
Dazaev Vives <dazaev.vives.cervantes@gmail.com>
2017-10-19 20:16:11 -0500

    restaurant page now working

--- public/scripts/restaurants.js
+++ public/scripts/restaurants.js
@@ -10,19 +10,28 @@ $(document).ready(function() {
 	});
 });
 
+// var keys = document.querySelectorAll(".keys");
+//
+// keys.forEach(function (data){
+// 	if (true) {
+//
+// 	} else {
+//
+// 	}
+// });
+
 function onSuccess(data) {
 	console.log(data);
 	for (var i = 0; i < data.length ; i++) {
-		$('#main').append('<div class = "name"><br>'+data[i].name+'</div>');
-		$('#main').append('<div class = "address">'+data[i].address+'</div>');
-		$('#main').append('<div class = "phoneNum">'+data[i].phoneNum+'</div>');
-		$('#main').append('<div class = "email">'+data[i].email+'</div>');
-		$('#main').append('<div class = "foodHeader">Food Available: <br></div>');
+		$('#main').append('<div class = "name keys"><br>'+data[i].name+'</div>');
+		$('#main').append('<div class = "address keys">'+data[i].address+'</div>');
+		$('#main').append('<div class = "phoneNum keys">'+data[i].phoneNum+'</div>');
+		$('#main').append('<div class = "email keys">'+data[i].email+'</div>');
+		$('#main').append('<div class = "foodHeader keys">Food Available: <br></div>');
 
-		// $('#main').append('<div class = "foodLeft">'+data[i].foodLeft[0].weight+' of '+data[i].foodLeft[0].name+' prepared on '+data[i].foodLeft[0].datePrepared+'</div>');
-		// 	for (var x = 1; x < data[i].foodLeft.length ; x++) {
-		// 		$('#main').append('<div class = "foodLeft">'+data[i].foodLeft[x].weight+' of '+data[i].foodLeft[x].name+' prepared on '+data[i].foodLeft[x].datePrepared+'</div>');
-		// };
+			for (var x = 1; x < data[i].foodLeft.length ; x++) {
+				$('#main').append('<div class = "foodLeft">'+data[i].foodLeft[x].weight+' of '+data[i].foodLeft[x].name+' prepared on '+data[i].foodLeft[x].datePrepared+'</div>');
+		};
 		var restUrlId = data[i]._id;
 		$('#main').append(`<button class="foodButton" type="submit"><a data-id="${restUrlId}" href="/restaurants/${restUrlId}/food">Add Food</a></button><br><br><br>`);
 
